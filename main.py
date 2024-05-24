60cf0c5cb422727be052827a6dbcfbcba06ea172
SP-Ulluri <ulluri@live.co.uk>
2024-05-24 12:32:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-import os
-import base64
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from email.mime.text import MIMEText
 import streamlit as st
-import json
-from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-import tempfile
-from datetime import datetime, timedelta
-import base64
 from streamlit_extras.switch_page_button import switch_page
 from app_functions import hide_st_ui, authenticate
 
@@ -22,46 +8,6 @@ st.set_page_config(
 )
 
 hide_st_ui()
-
-def get_unread_email_details(credentials, email_index):
-    service = build('gmail', 'v1', credentials=credentials)
-    try:
-        page_token = None
-        while True:
-            results = service.users().messages().list(userId='me', labelIds=['INBOX', 'UNREAD'],
-                                                      pageToken=page_token).execute()
-            messages = results.get('messages', [])
-            if not messages:
-                break
-            if len(messages) > email_index:
-                message_id = messages[email_index]['id']
-                message_data = service.users().messages().get(userId='me', id=message_id).execute()
-                # st.write(message_data)
-                payload = message_data['payload']
-                headers = payload['headers']
-                email_details = {}
-                for header in headers:
-                    if header['name'] == 'From':
-                        email_details['From'] = header['value']
-                    elif header['name'] == 'Subject':
-                        email_details['Subject'] = header['value']
-                # Get email body
-                if 'parts' in payload:
-                    for part in payload['parts']:
-                        if part['mimeType'] == 'text/plain':
-                            data = part['body']['data']
-                            email_body = base64.urlsafe_b64decode(data).decode('utf-8')
-                            email_details['Body'] = email_body
-                            break  # Stop processing parts after finding the text/plain part
-                return [email_details]
-            else:
-                page_token = results.get('nextPageToken')
-                if not page_token:
-                    break
-    except HttpError as e:
-        st.error(f'An error occurred: {e}')
-    else:
-        return None
         
 def main():
     st.header("Welcome to your Simpler Inbox")
